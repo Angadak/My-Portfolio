cf708518a80241d154278975a34a1527c6d9d8dc
Angadak <angad.ak70@gmail.com>
2024-02-16 10:47:12 +0530

    changed dev img

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,7 +81,7 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage:{
-        hero:'url(/hero/hero-bg.png)',
+        hero:'url(/hero/profile-1.png)',
         hero_shape:'url(/hero/shape-1.svg)',
         hero_shape1_light:'url(/hero/shape-2-light.svg)',
         hero_shape2_dark:'url(/hero/shape-2-dark.svg)',
